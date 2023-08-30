162b1980af55d6f5305e08503abe87b4e18a34cc
ellen3128 <hyunpark.ellen@gmail.com>
2023-08-30 15:49:34 -0400

    updates language skills on about page

--- react-portfolio/src/pages/Skills.js
+++ react-portfolio/src/pages/Skills.js
@@ -1,10 +1,17 @@
-
-export default function Skills () {
-    return (
-        <section className="skill-cards">
-        <div className="skill-languages"> Languages </div>
-        <div className="skill-utilities"> Utilities </div>
-        <div className="skill-interests"> Interests </div>
-      </section>
-    )
+export default function Skills() {
+  return (
+    <section className="skill-cards">
+      <div className="skill-languages">
+        <span className="language-label">Languages </span>
+        <div></div>
+        <span className="language">HTML</span>
+        <span className="language">CSS</span>
+        <span className="language">JavaScript</span>
+        <span className="language">React</span>
+        <span className="language">Python</span>
+      </div>
+      <div className="skill-utilities"> Frameworks && Utilities </div>
+      <div className="skill-interests"> Interests </div>
+    </section>
+  );
 }
